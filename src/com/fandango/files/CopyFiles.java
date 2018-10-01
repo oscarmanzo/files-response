b4616375a65a694daef3f791766c09013adafbf2
oscar.manzo.c@gmail.com <oscar.manzo.c@gmail.com>
2018-10-01 01:04:39 -0500

    Cambios

--- src/com/fandango/files/CopyFiles.java
+++ src/com/fandango/files/CopyFiles.java
@@ -1,13 +1,10 @@
 package com.fandango.files;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class CopyFiles {
 
--- src/com/fandango/files/ParseFile.java
+++ src/com/fandango/files/ParseFile.java
@@ -1,64 +1,121 @@
 package com.fandango.files;
 
-import org.apache.commons.lang.math.NumberUtils;
-
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+import static java.util.stream.Collectors.*;
 
 public class ParseFile {
 
-    private static final String LOG_FILE = "C:\\Users\\oscar.manzo\\Globant\\Fandango\\Services\\TO_REBASE\\pos-service\\logs\\posservice-client.log";
+    private static final String ROOT = "C:\\Users\\oscar\\Documents\\Proyectos\\Globant\\Fandango\\Master\\pos-service\\src\\wiremock\\resources\\com\\fandango\\commerce\\posresponse\\__files\\";
+
+    private static final String LOG_FILE = "C:\\Users\\oscar\\Documents\\Proyectos\\Globant\\Fandango\\Master\\pos-service\\logs\\posservice-app-access.log";
 
     private static final String FIND_PERFORMANCE = "posPerformanceCode";
     private static final String FIND_FILE_NAME = "bodyFileName";
 
+    private static final String CODE_XXX = "XXX";
+    private static final String CODE_DEFAULT = "0000";
+    private static final String DELIMITER = "####################################################";
+
     public static final void main(String[] args) {
         try {
             ParseFile app = new ParseFile();
-            app.run();
+            List<FileVO> files = app.run();
+
+            //app.renameFile(files);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void run() throws Exception {
-        List<String> values = parseLog(new File(LOG_FILE));
-        print(values);
+    private List<FileVO> run() throws Exception {
+        File file = new File(LOG_FILE);
 
-        System.out.println("####################################################");
+        List<String> blocks = readFileDelimeter(file);
+        List<TestVO> tests = parseBlocks(blocks);
 
-        Map<String, List<String>> map = fillMap(values);
-        map.forEach((k,v) -> System.out.println(k));
+        tests = tests.stream()
+                .filter(vo -> vo.files.size()<50)
+                .collect(toList());
 
-        Map<String, List<String>> groups = groupByPerformance(values, map);
-        print(groups);
+        //tests.forEach(System.out::println);
+        //System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        List<FileVO> files = tests.stream()
+                .filter(vo -> vo.files.size()<50)
+                .flatMap(vo -> listFiles(vo).stream())
+                .collect(toList());
+
+        //files.forEach(System.out::println);
+        //System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        files = files.stream().distinct().collect(toList());
+        files.forEach(System.out::println);
+
+        return files;
     }
 
-    private List<String> parseLog(File logFile) throws Exception {
-        List<String> values = new ArrayList<>();
+    private List<String> readFileDelimeter(File logFile) throws Exception {
+        List<String> chunks = new ArrayList<>();
 
         Scanner sc = new Scanner(logFile);
+        sc.useDelimiter(DELIMITER);
+
+        while (sc.hasNext()){
+            chunks.add(sc.next());
+        }
+
+        return chunks;
+    }
+
+    private List<TestVO> parseBlocks(List<String> blocks) {
+        return blocks.stream()
+                .map( b -> parseBlock(b))
+                .collect(toList());
+    }
+
+    private TestVO parseBlock(String block) {
+        List<String> codes = new ArrayList<>();
+        List<String> files = new ArrayList<>();
+
+        Scanner sc = new Scanner(block);
 
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
 
             Optional<String> performanceOpt = findText(line, FIND_PERFORMANCE);
             if (performanceOpt.isPresent())
-                values.add(performanceOpt.get());
+                codes.add(performanceOpt.get());
 
             Optional<String> filenameOpt = findText(line, FIND_FILE_NAME);
             if (filenameOpt.isPresent())
-                values.add(filenameOpt.get());
+                files.add(filenameOpt.get());
         }
 
-        return values;
+        String code = getRightCode(codes);
+        return new TestVO(code, files);
+    }
+
+    private String getRightCode(List<String> codes){
+        if (codes==null || codes.isEmpty()) return CODE_DEFAULT;
+
+        if (codes.size()==1) return codes.get(0);
+
+        return codes.stream()
+                .filter(c -> !c.trim().isEmpty())
+                .reduce((a, b) -> b).orElse(CODE_DEFAULT);
     }
 
     private Optional<String> findText(String textline, String toFind) {
         int index = textline.indexOf(toFind);
 
         if (index >= 0) {
-            System.out.println("------------>"+ textline);
+            //System.out.println("------------>"+ textline);
             String aux = FIND_PERFORMANCE.equals(toFind) ? "\":\"" : "\" : \"";
             return parse(textline.substring(index), toFind, aux);
         }
@@ -73,7 +130,7 @@ public class ParseFile {
             String value = subaux.substring(0, subaux.indexOf("\""));
 
             if (FIND_PERFORMANCE.equals(toFind) && (value==null || value.trim().isEmpty()))
-                value = "0";
+                value = CODE_DEFAULT;
 
             return Optional.of(value);
         } catch (Exception e) {
@@ -83,47 +140,114 @@ public class ParseFile {
         return Optional.empty();
     }
 
-    private Map<String, List<String>> groupByPerformance(List<String> values, Map<String, List<String>> map){
-        Map<List<String>, List<String>> groups = new HashMap<>();
-        Stack<String> stack = new Stack<>();
+    private List<FileVO> listFiles(TestVO vo){
+        List<FileVO> files = new ArrayList<>();
 
-        for (String value : values){
-            if (!NumberUtils.isNumber(value)){
-                stack.push(value);
-            } else {
-                List<String> files = map.get(value);
-
-                while (!stack.isEmpty()){
-                    files.add(stack.pop());
-                }
-            }
+        for (String filename : vo.files){
+            files.add(new FileVO(vo.performanceCode, filename, replace(filename, vo.performanceCode)));
         }
 
-        return map;
+        return files;
     }
 
-    private Map<String, List<String>> fillMap(List<String> values) {
-        Map<String, List<String>> map = new HashMap<>();
+    private String replace(String filename, String pc){
 
-        values.stream()
-                .filter(v -> NumberUtils.isNumber(v))
-                .distinct()
-                .forEach(s -> map.put(s, new ArrayList<String>()));
+        int last = filename.lastIndexOf("/");
+        if (last>0){
+            filename = filename.substring(last + 1);
+        }
 
-        return map;
+        if (filename.contains(CODE_XXX)){
+            return filename.replace(CODE_XXX, pc);
+        }
+
+        if (!filename.contains(pc)){
+            return replaceCP(filename, pc);
+        }
+
+        return filename;
     }
 
-    private void print(List<String> values){
-        values.forEach(v -> System.out.println(v));
+    private String replaceCP(String file, String pc){
+        int one = file.indexOf("_");
+        int two = file.indexOf("_", one + 1);
+        int three = file.indexOf("_", two + 1);
+
+        if (three<0) three = file.indexOf(".");
+
+        String a = file.substring(0, two + 1);
+        String b = file.substring(three);
+        String c = a + pc + b;
+
+        return c;
     }
 
-    private void print(Map<String, List<String>> groups){
-        groups.forEach((key,vs) -> {
-            //System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            for (String value : vs){
-                System.out.println(key +"\t"+ value);
-            }
-        });
+    private void renameFile(List<FileVO> vos){
+        vos.forEach(v -> renameFile(v));
+    }
+
+    private void renameFile(FileVO vo){
+        File file = new File(ROOT + vo.file);
+
+        System.out.println(file.getPath());
+        System.out.println(file.exists());
+        System.out.println(file.getParent());
+
+        File newfile = new File(file.getParent() +"\\"+ vo.newName);
+        System.out.println(newfile.getPath());
+        file.renameTo(newfile);
+    }
+
+    class TestVO {
+        String performanceCode;
+        List<String> files;
+
+        private TestVO(String performanceCode, List<String> files){
+            this.performanceCode = performanceCode;
+            this.files = files;
+        }
+
+        @Override
+        public String toString() {
+            return performanceCode +":\n"+
+                    files.stream()
+                            .collect(toSet())
+                            .stream()
+                            .collect(joining(",'\n"));
+        }
+    }
+
+    class FileVO {
+        String code;
+        String file;
+        String newName;
+
+        private FileVO(String code, String file, String newName){
+            this.code = code;
+            this.file = file;
+            this.newName = newName;
+        }
+
+        public boolean isChanged() {
+            return file==null || newName==null? false : !file.endsWith(newName);
+        }
+
+        @Override
+        public String toString() {
+            return this.code +"\t"+ this.file +"\t"+ this.newName +"\t"+ isChanged();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            FileVO other = (FileVO)obj;
+            return (this.code + this.file).equals((other.code + other.file));
+        }
+
+        @Override
+        public int hashCode() {
+            return (this.code + this.file).hashCode();
+        }
+
     }
 
     /*private Optional parseCode(String text, String field){
@@ -138,4 +262,5 @@ public class ParseFile {
         }
         return Optional.empty();
     }*/
+
 }
